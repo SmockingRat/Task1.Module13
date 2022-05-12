8af59b3196c4ea1bd2d4f9f86aecd9b19dbbbb9a
SmokingRat <halfbrikbarry@gmail.com>
2022-05-12 22:11:18 +0300

    Добавьте файлы проекта.

---
+++ Task1.Module13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace Task1.Module13
+{
+    /// <summary>
+    /// Central  class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Central method
+        /// </summary>
+        /// <param name="args"> Entry paprameter </param>
+        static void Main(string[] args)
+        {
+            ////Adding two lists
+            List<string> nlist = new List<string>();
+            LinkedList<string> llist = new LinkedList<string>();
+
+            ////Adding URL of file and time counter
+            StreamReader read = new StreamReader("C:\\Users\\Михаил\\Downloads\\Text1.txt");
+            Stopwatch swatch = Stopwatch.StartNew();
+
+            ////First list
+            while (!read.EndOfStream)
+            {
+                nlist.Add(read.ReadLine());
+            }
+
+            double fnum = swatch.Elapsed.TotalMilliseconds;
+            Console.WriteLine($"Время первой операции - {fnum}");
+
+            ////Second list
+            while (!read.EndOfStream)
+            {
+                llist.AddLast(read.ReadLine());
+            }
+
+            double snum = swatch.Elapsed.TotalMilliseconds;
+            Console.WriteLine($"Время первой операции - {snum}");
+
+            ////Difference in time
+            Console.WriteLine($"Разница - {snum - fnum}");
+            Console.ReadLine();
+        }
+    }
+}
